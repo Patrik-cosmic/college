2a397ae30b37ead3382cea8f3ef6ec24eeca9833
Patrik-cosmic <sumande2019@outlook.com>
2021-10-02 22:35:43 +0530

    Day3 Intro

--- common/models.py
+++ common/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    content = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.content}'
+
+    # class Meta:
+    #     verbose_name_plural = 'Quote'
--- common/views.py
+++ common/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Quote
+import datetime
 # Create your views here.
 def index(request):
+    # res = Quote.objects.all()
+    # print(type(res[0]))
+    # print(type(res[1]))
+
+    # for quotes in res:
+    #     print(quotes.content)
+
     # return HttpResponse("Welcome to CTCJ college")
-    return render(request, 'college/home.html')
+    context = {
+        'data': Quote.objects.all(),
+        'comment_date': datetime.date.today(),
+    }
+    return render(request, 'college/home.html', context=context)
