8cc0dbfd89feb3d35878575f6442737444269023
Patrik-cosmic <sumande2019@outlook.com>
2021-10-10 20:53:44 +0530

    Day3 Intro

--- common/models.py
+++ common/models.py
@@ -6,7 +6,7 @@ class Quote(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.id}: {self.content}'
+        return f'{self.id}: {self.content} : {self.date_created}'
 
     # class Meta:
     #     verbose_name_plural = 'Quote'
